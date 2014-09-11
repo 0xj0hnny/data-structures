6bd72b25dfe56af53433621f5b4abe1e0cfe0374
johnnyliang213 <zuyanliang@gmail.com>
2014-09-11 11:32:36 -0700

    passed prototypal and pseudoclassical

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.storage = {};
+  stack.count = 0;
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push:function(value){
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  pop: function(){
+    this.count && this.count--;
+    var temp = this.storage[this.count];
+    var out = temp;
+    delete temp;
+    return out;
+  },
+  size:function(){
+    return this.count;
+  }
+};
 
 
