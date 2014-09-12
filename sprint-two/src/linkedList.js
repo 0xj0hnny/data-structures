1348f0ba08cebc5879ba386c11db3f7e788a50dc
johnnyliang213 <zuyanliang@gmail.com>
2014-09-11 22:06:25 -0700

    passed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,47 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if(list.head === null){
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      var prevNode = list.tail;
+      list.tail = newNode;
+      prevNode.next = list.tail;
+    }
   };
 
   list.removeHead = function(){
+    if(list.head){
+      var headNode = list.head;
+      list.head = headNode.next;
+      return headNode.value;
+    }
   };
 
   list.contains = function(target){
+    var result = false;
+    if(list.head === null){
+      return result
+    }
+
+    var check = function(node) {
+      if(node.value === target){
+        result = true
+        return result
+      }
+      else if (node.next === null){
+          return result
+      }else{
+         check(node.next);
+      }
+    }
+
+    check(list.head);
+
+    return result
+
   };
 
   return list;
