f64e2f51b0bd0e9ddb8f4f25f457581518e0ce28
johnnyliang213 <zuyanliang@gmail.com>
2014-09-12 22:38:38 -0700

    start working on binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,26 @@
 var makeBinarySearchTree = function(value){
+  var binarySearchTree = {};
+
+  var left = undefined;
+  var right = undefined;
+
+  binarySearchTree.insert = function(value){
+
+  };
+
+  binarySearchTree.contains = function(value){
+
+  };
+
+  binarySearchTree.depthFirstLog = function(callback){
+
+  };
+
+  return binarySearchTree;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
