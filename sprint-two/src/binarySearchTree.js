e146d208ec7b0b7c87423f8ad80d4d0545fee105
johnnyliang213 <zuyanliang@gmail.com>
2014-09-13 11:54:06 -0700

    finished bTree and graph

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,19 +1,92 @@
 var makeBinarySearchTree = function(value){
   var binarySearchTree = {};
 
-  var left = undefined;
-  var right = undefined;
+  binarySearchTree.left = undefined;
+  binarySearchTree.right = undefined;
+  binarySearchTree.value = value;
 
-  binarySearchTree.insert = function(value){
+  binarySearchTree.insert = function(num){
+    //make a new tree
+    //check if num is > current.value
+      //check current.right exist
+      //call inst on current right
+    //if num is < current.value
+      //check current.left exist
+      //call inst on current left
+    //call inst with this
+    var newTree = makeBinarySearchTree(num);
+    var curTree = this;
+    var inst = function(tree){
+      if(tree.value > num){
+        if(tree.left){
+          inst(tree.left);
+        } else {
+          tree.left = newTree;
+        }
+      } else {
+        if(tree.right){
+          inst(tree.right);
+        } else {
+          tree.right = newTree;
+        }
+      }
+
+    }
+
+    inst(curTree);
 
   };
 
-  binarySearchTree.contains = function(value){
+  binarySearchTree.contains = function(num){
+    //declare result with false
+    //make a recursion funciton check
+      //check current tree value equals to the num
+      //if yes, change result to true and return
+      //check if current tree value greater than num
+        //if so check left exist
+        //call check with left tree
+      //check the right
+        //if right exist
+        //call check with right tree
+    //call check function with this
+    //return result
+
+    var result = false;
+    var check = function(tree){
+      if(tree.value === num){
+        result = true;
+        return result;
+      } else {
+        if(tree.value > num){
+          if(tree.left){
+            check(tree.left);
+          }
+        } else {
+          if(tree.right){
+            check(tree.right);
+          }
+        }
+      }
+    };
+    check(this);
+    return result;
 
   };
 
   binarySearchTree.depthFirstLog = function(callback){
-
+    //call callback on the current tree.value
+    //check this tree has left
+    //if so callback on the left
+    // check this tree has right
+    //if so callback on the right
+    // debugger;
+    callback(this.value);
+    if(this.left){
+      this.left.depthFirstLog(callback);
+    }
+    if(this.right){
+      this.right.depthFirstLog(callback);
+    }
   };
 
   return binarySearchTree;
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,22 +1,76 @@
 var Graph = function(){
+  this.storage = {};
+  this.count = 0;
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+
+  for(var i = 0; i < arguments.length; i++){
+    if(!this.storage[arguments[i]]){
+     this.storage[arguments[i]] = { value:arguments[i], edge:[]};
+    }
+  }
+
+  var arr = [];
+  for(var key in this.storage){
+    arr.push(key);
+  }
+  if(arr.length === 2){
+    this.addEdge(arr[0], arr[1]);
+
+  }
+
+  if(arguments[1]){
+    this.addEdge(newNode, toNode);
+  }
+
+  //if argument is 1,
+  //than create a newnode
+  //if argument is 2,
+  //call add edge
+
 };
 
 Graph.prototype.contains = function(node){
+  //check the storage for the value node
+  return this.storage[node] ? true : false;
 };
 
 Graph.prototype.removeNode = function(node){
+  if(this.storage[node]){
+    for(var i = 0; i < this.storage[node].edge.length; i++){
+      var edgeTo = this.storage[node].edge[i];
+      var edgeNode = this.storage[edgeTo];
+      edgeNode.edge.splice(degeNode.edge.indexOf(node),1);
+    }
+    delete this.storage[node];
+  }
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
+
+  return this.storage[fromNode].edge.indexOf(toNode) !== -1 ? true : false;
+
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.storage[fromNode].edge.push(toNode);
+  this.storage[toNode].edge.push(fromNode);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  var edgeTo = this.storage[fromNode].edge;
+  edgeTo.splice(edgeTo.indexOf(toNode),1);
+  var edgeFrom = this.storage[toNode].edge;
+  edgeFrom.splice(edgeFrom.indexOf(fromNode),1);
+
+  if(this.storage[fromNode].edge.length === 0){
+    delete this.storage[fromNode];
+  }
+  if(this.storage[toNode].edge.length === 0){
+    delete this.storage[toNode];
+  }
+  console.log(this.storage);
 };
 
 /*
